00795e74eee8963079f206b2f3e9cf5be33a8cb7
curso06 <nocorreo@mocorreo.com>
2025-03-06 12:11:57 +0100

    eliminado documento y expediente

--- src/main/java/es/santander/ascender/ejerc008/repository/DocumentoRepository.java
+++
@@ -1,9 +0,0 @@
-package es.santander.ascender.ejerc008.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.santander.ascender.ejerc008.model.Documento;
-
-public interface DocumentoRepository extends JpaRepository<Documento, Long> {
-
-}
--- src/main/java/es/santander/ascender/ejerc008/repository/ExpedienteRepository.java
+++
@@ -1,9 +0,0 @@
-package es.santander.ascender.ejerc008.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.santander.ascender.ejerc008.model.Expediente;
-
-public interface ExpedienteRepository extends JpaRepository<Expediente, Long> {
-
-}
